b7fc633de48b9fefc8a2ccc80f9c578ef5a0f642
Priyadharshini M <m.priyadharshinimanak@gmail.com>
2022-09-22 10:38:03 +0530

    API url is changed

--- src/main/java/com/apiexample/springapiexample/SpringAPIController.java
+++ src/main/java/com/apiexample/springapiexample/SpringAPIController.java
@@ -4,8 +4,6 @@ package com.apiexample.springapiexample;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,13 +11,13 @@ import java.util.HashMap;
 public class SpringAPIController {
 
     HashMap<String,String>log=new HashMap<>();
-    @PostMapping("/postDetail")
+    @PostMapping("/Users")
     public ResponseEntity<Object> create(@RequestBody UserInfo post){
         log.put(post.Name,post.Password);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(log);
     }
 
-    @GetMapping("/pass/{userName}")
+    @GetMapping("/Users/{userName}")
     public ResponseEntity<Object> getpass(@PathVariable String userName){
         ArrayList user=new ArrayList(log.keySet());
         if(user.contains(userName)){
@@ -28,7 +26,7 @@ public class SpringAPIController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Not found");
     }
 
-    @DeleteMapping("/delete/{userName}")
+    @DeleteMapping("/Users/{userName}")
     public ResponseEntity<Object> del(@PathVariable String userName){
         if(log.containsKey(userName)){
             log.remove(userName);
@@ -37,7 +35,7 @@ public class SpringAPIController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Not Found");
     }
 
-    @PutMapping("/update/{userName}")
+    @PutMapping("/Users/{userName}")
     public ResponseEntity<Object> upd(@PathVariable String userName,@RequestBody UserInfo n){
         if(log.containsKey(userName)){
             log.put(userName,n.Password);
